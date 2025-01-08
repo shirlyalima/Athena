eb856be414e536877f68e4b6933eed78a2e42b69
Shirlya <shirlya@hotmail.com.br>
2025-01-08 14:41:28 -0300

    Add files via upload

--- Athena/script.js
+++ Athena/script.js
@@ -1,7 +1,11 @@
 const athena = document.querySelector('.athena');
 const column = document.querySelector('.column');
-const moves = document.getElementById("moves-count");
-const timeValue = document.getElementById("time");
+const time = document.getElementById("time");
+const botaoReiniciar = document.getElementById("reiniciar");
+
+
+let tempoAtual = 100;
+let checarClock;
 
 const jump = () => {
     athena.classList.add('jump');
@@ -29,34 +33,34 @@ const loop = setInterval(() => {
         athena.style.marginLeft = '200px';
 
         clearInterval(loop);
+        clearInterval(checarClock);
     }
 }, 10);
 
+
+function clock() {
+    tempoAtual--;
+    time.textContent = tempoAtual;
+    if (tempoAtual === 0) {
+        alert("Time is over!")
+        removerTeclas();
+        clearInterval(checarClock);
+
+    }
+
+}
+
+function removerTeclas() {
+    document.removeEventListener("keydown", jump);
+}
+
+
+botaoReiniciar.addEventListener('click', function () {
+  location.reload(); 
+    
+})
+
+
+
 document.addEventListener('keydown', jump);
-
-
-// //Initial Time
-// let seconds = 0,
-//   minutes = 0;
-// //Initial moves and win count
-// let movesCount = 0,
-//   winCount = 0;
-// //For timer
-// const timeGenerator = () => {
-//   seconds += 1;
-//   //minutes logic
-//   if (seconds >= 60) {
-//     minutes += 1;
-//     seconds = 0;
-//   }
-//   //format time before displaying
-//   let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
-//   let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
-//   timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
-// };
-
-// //For calculating moves
-// const movesCounter = () => {
-//   movesCount += 1;
-//   moves.innerHTML = `<span>Movimentos:</span>${movesCount}`;
-// };
+checarClock = setInterval(clock, 1000);
